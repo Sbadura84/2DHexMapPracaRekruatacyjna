47703fa3f1e051e5e2d2c7fc7e705d6843fa758e
MrocznyKnajt <sbadura84@gmail.com>
2023-02-04 14:26:33 +0100

    Camera movement completed
    Completed camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,11 +6,14 @@ public class CameraMovement : MonoBehaviour
 {
     private float horizontalInput;
     private float verticalInput;
+    private float horizontalSum;
+    private float verticalSum;
     private float horizontalSpeed;
     private float verticalSpeed;
 
     [SerializeField] private Camera cam;
     private Vector3 originPoint;
+    [SerializeField] private float mouseSensitivity = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +26,30 @@ public class CameraMovement : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(1))
         {
+            Cursor.lockState = CursorLockMode.Locked;
+            originPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            horizontalSum = 0;
+            verticalSum = 0;
+
+        } else if (Input.GetMouseButton(1)){
+            Cursor.lockState = CursorLockMode.Locked;
+            horizontalInput = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            verticalInput = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+            horizontalSum = horizontalSum + horizontalInput;
+            verticalSum = verticalSum + verticalInput;
+            cam.transform.position += new Vector3(horizontalSum, verticalSum, 0);
+            
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            Debug.Log("release");
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        
+
+        /*
+        if (Input.GetMouseButtonDown(1))
+        {
             originPoint = cam.ScreenToWorldPoint(Input.mousePosition);
             Debug.Log(originPoint);
         }
@@ -32,6 +59,6 @@ public class CameraMovement : MonoBehaviour
             float differenceX = originPoint.x - cam.ScreenToWorldPoint(Input.mousePosition).x;
         cam.transform.position += new Vector3(differenceX,differenceY,0);
         }
-
+        */
     }
 }
